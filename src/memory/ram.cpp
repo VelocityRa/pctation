4d75e8558c1474dfac94c285fa6bd48c13f3d22f
Nick Renieris <velocityra@gmail.com>
2019-04-14 21:35:52 +0300

    memory/ram: PS-X EXE loading (partial)

--- src/memory/ram.cpp
+++ src/memory/ram.cpp
@@ -1,11 +1,16 @@
 #include <memory/ram.hpp>
 
+#include <util/load_file.hpp>
+#include <util/log.hpp>
+
+#include <gsl-lite.hpp>
+
 #include <algorithm>
 #include <array>
 
 namespace memory {
 
-Ram::Ram() {
+Ram::Ram(fs::path psxexe_path) : m_psxexe_path(psxexe_path) {
   m_data = std::make_unique<std::array<byte, memory::RAM_SIZE>>();
   m_data_scratch = std::make_unique<std::array<byte, memory::SCRATCHPAD_SIZE>>();
 
@@ -56,6 +61,56 @@ void Ram::write16_scratch(u32 addr, u16 val) {
 }
 void Ram::write8_scratch(u32 addr, u8 val) {
   m_data_scratch.get()->data()[addr] = val;
+}
+
+bool Ram::load_executable(PSEXELoadInfo& out_psx_load_info) {
+  if (m_psxexe_path.empty())
+    return false;
+
+  const buffer& psx_exe_buf = util::load_file(m_psxexe_path);
+
+  if (psx_exe_buf.empty())
+    return false;
+
+  struct PSXEXEHeader {
+    char magic[8];  // "PS-X EXE"
+    u8 pad0[8];
+    u32 pc;         // initial PC
+    u32 r28;        // initial R28
+    u32 load_addr;  // destination address in RAM
+    u32 filesize;   // excluding header & must be N*0x800
+    u32 unk0[2];
+    u32 memfill_start;
+    u32 memfill_size;
+    u32 r29_r30;         // initial r29 and r30 base
+    u32 r29_r30_offset;  // initial r29 and r30 offset, added to above
+    // etc, we don't care about anything else
+  };
+
+  const auto psx_exe = (PSXEXEHeader*)psx_exe_buf.data();
+
+  if (std::strcmp(&psx_exe->magic[0], "PS-X EXE")) {
+    LOG_ERROR("Not a valid PS-X EXE file!");
+    return false;
+  }
+
+  // We don't support memfill
+  Ensures(psx_exe->memfill_start == 0);
+  Ensures(psx_exe->memfill_size == 0);
+
+  out_psx_load_info.pc = psx_exe->pc;
+  out_psx_load_info.r28 = psx_exe->r28;
+  out_psx_load_info.r29_r30 = psx_exe->r29_r30 + psx_exe->r29_r30_offset;
+
+  constexpr auto PSXEXE_HEADER_SIZE = 0x800;
+
+  const auto copy_src_begin = psx_exe_buf.data() + PSXEXE_HEADER_SIZE;
+  const auto copy_src_end = copy_src_begin + psx_exe->filesize;
+  const auto copy_dest_begin = m_data->data() + (psx_exe->load_addr & 0x7FFFFFFF);
+
+  std::copy(copy_src_begin, copy_src_end, copy_dest_begin);
+
+  return true;
 }
 
 }  // namespace memory
--- src/memory/ram.hpp
+++ src/memory/ram.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <memory/map.hpp>
+#include <util/fs.hpp>
 #include <util/types.hpp>
 
 #include <array>
@@ -8,19 +9,37 @@
 
 namespace memory {
 
+struct PSEXELoadInfo {
+  u32 pc;
+  u32 r28;
+  u32 r29_r30;
+};
+
 class Ram {
  public:
-  Ram();
+  explicit Ram(fs::path psxexe_path);
   u32 read32(u32 addr) const;
   u16 read16(u32 addr);
   u8 read8(u32 addr) const;
   void write32(u32 addr, u32 val);
   void write16(u32 addr, u16 val);
   void write8(u32 addr, u8 val);
+  bool load_executable(PSEXELoadInfo& out_psx_load_info);  // Returns true on successful load
   const std::array<byte, RAM_SIZE>& data() const { return *m_data; }
+
+  // Scratchpad memory
+  // TODO: refactor
+  u32 read32_scratch(u32 addr) const;
+  u16 read16_scratch(u32 addr);
+  u8 read8_scratch(u32 addr) const;
+  void write32_scratch(u32 addr, u32 val);
+  void write16_scratch(u32 addr, u16 val);
+  void write8_scratch(u32 addr, u8 val);
 
  private:
   std::unique_ptr<std::array<byte, RAM_SIZE>> m_data;
+  std::unique_ptr<std::array<byte, SCRATCHPAD_SIZE>> m_data_scratch;
+  fs::path m_psxexe_path;
 };
 
 }  // namespace memory
