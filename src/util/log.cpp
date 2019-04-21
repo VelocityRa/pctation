1a25c31cdfd426951c113cea6d5955d069657fcb
Nick Renieris <velocityra@gmail.com>
2019-04-21 19:41:36 +0300

    util/log: Add CPU logger

--- src/util/log.cpp
+++ src/util/log.cpp
@@ -8,30 +8,40 @@
 #include <string>
 #include <vector>
 
-constexpr auto LOG_FILENAME =
+// TODO: Clean up
 #ifdef WIN32
-    L"pctation.log";
+constexpr auto LOG_FILENAME = L"pctation.log";
+constexpr auto LOG_CPU_FILENAME = L"pctation_cpu.log";
 #else
-    "pctation.log";
+constexpr auto LOG_FILENAME = "pctation.log";
+constexpr auto LOG_CPU_FILENAME = "pctation_cpu.log";
 #endif
 
 namespace logging {
+
+std::shared_ptr<spdlog::logger> g_cpu_logger;
 
 void init() {
   // Set up sinks
   std::vector<spdlog::sink_ptr> sinks;
   sinks.push_back(std::make_shared<spdlog::sinks::stdout_color_sink_st>());
-  //  sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_st>(LOG_FILENAME, true));
+  sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_st>(LOG_FILENAME, true));
 
-  // Set up logger
+  // Set up generic logger
   auto pctation_logger = std::make_shared<spdlog::logger>("pctation", begin(sinks), end(sinks));
-  pctation_logger->flush_on(spdlog::level::err);
-  spdlog::flush_every(std::chrono::seconds(1));
+  pctation_logger->set_level(spdlog::level::debug);
+  pctation_logger->flush_on(spdlog::level::debug);
+  pctation_logger->set_pattern("%^[--%L--] %16s:%-3# %v%$");
+  spdlog::register_logger(pctation_logger);
+
+  // Set up CPU logger
+  g_cpu_logger = spdlog::basic_logger_st("pctation_cpu", LOG_CPU_FILENAME, true);
+  g_cpu_logger->set_level(spdlog::level::trace);
+  g_cpu_logger->flush_on(spdlog::level::trace);
+  g_cpu_logger->set_pattern("%v");
 
   // Configure spdlog
   spdlog::set_default_logger(pctation_logger);
-  spdlog::set_pattern("%^[--%L--] %16s:%-3# %v%$");
-  spdlog::set_level(spdlog::level::debug);
 }
 
 }  // namespace logging
--- src/util/log.hpp
+++ src/util/log.hpp
@@ -1,5 +1,8 @@
 #pragma once
 
+#define SPDLOG_ACTIVE_LEVEL SPDLOG_LEVEL_TRACE
+
+#include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/spdlog.h>
 
 #define LOG_TRACE SPDLOG_TRACE
@@ -11,8 +14,13 @@
 
 #define LOG_TODO() LOG_WARN(__FUNCTION__ ": TODO")
 
+#define LOG_CPU(...) SPDLOG_LOGGER_TRACE(logging::g_cpu_logger, __VA_ARGS__)
+#define LOG_CPU_NOFMT(msg) logging::g_cpu_logger->trace(msg)
+
 namespace logging {
+
+extern std::shared_ptr<spdlog::logger> g_cpu_logger;
 
 void init();
 
-}
+}  // namespace logging
