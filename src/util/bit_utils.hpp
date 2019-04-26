8d0bf22c68dea7aef5684bab41b527232fa4fa47
Nick Renieris <velocityra@gmail.com>
2019-04-26 18:59:12 +0300

    util/bit_utils: Sign extension helper function

---
+++ src/util/bit_utils.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include <util/types.hpp>
+
+namespace util {
+
+template <u32 sign_bit, typename T>
+T sign_extend(u64 n) {
+  static_assert(sign_bit > 0 && sign_bit < 63, "sign bit out of range");
+
+  T mask = (1LL << sign_bit) - 1;
+  const auto sign = (n & (1LL << sign_bit)) != 0;
+
+  T val = n & mask;
+
+  if (sign)
+    val |= ~mask;
+
+  return val;
+}
+
+}  // namespace util
