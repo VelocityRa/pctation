e7ba1fd444133a838870411da4038ae9fc52376e
Nick Renieris <velocityra@gmail.com>
2019-06-03 04:26:27 +0300

    cpu/gte: Implement most GTE commands

--- src/cpu/gte.cpp
+++ src/cpu/gte.cpp
@@ -7,6 +7,8 @@
 
 #include <algorithm>
 
+// Thanks to JaCzekanski (of Avocado) for some insights that make this code shorter/simpler
+
 namespace cpu {
 namespace gte {
 
@@ -37,7 +39,7 @@ u32 Gte::read_reg(u32 src_reg) {
     case 20: return col4_to_word(rgb_fifo[0]);
     case 21: return col4_to_word(rgb_fifo[1]);
     case 22: return col4_to_word(rgb_fifo[2]);
-    case 23: return col4_to_word(res);
+    case 23: return res;
     case 24: return mac[0];
     case 25: return mac[1];
     case 26: return mac[2];
@@ -147,7 +149,7 @@ void Gte::write_reg(u32 dest_reg, u32 val) {
     case 20: rgb_fifo[0] = col4_from_word(val); break;
     case 21: rgb_fifo[1] = col4_from_word(val); break;
     case 22: rgb_fifo[2] = col4_from_word(val); break;
-    case 23: res = col4_from_word(val); break;
+    case 23: res = val; break;
     case 24: mac[0] = val; break;
     case 25: mac[1] = val; break;
     case 26: mac[2] = val; break;
@@ -161,7 +163,7 @@ void Gte::write_reg(u32 dest_reg, u32 val) {
     case 29: break;
     case 30:
       lzcs = val;
-      lzcr = bit_utils::leading_zeroes(lzcs);
+      lzcr = (s32)bit_utils::leading_zeroes(lzcs);
       break;
     case 31:
       break;
@@ -239,12 +241,197 @@ void Gte::write_reg(u32 dest_reg, u32 val) {
   }
 }
 
+s32 Gte::clamp(s32 val, s32 min, s32 max, u32 flags) {
+  if (val > max) {
+    flag.word |= flags;
+    return max;
+  }
+  if (val < min) {
+    flag.word |= flags;
+    return min;
+  }
+  return val;
+}
+
+template <int BitSize>
+void Gte::check_ovf_and_udf(s64 val, u32 ovf_bit, u32 udf_bit) {
+  if (val >= (1LL << BitSize))
+    flag.word |= ovf_bit;
+  if (val < -(1LL << BitSize))
+    flag.word |= udf_bit;
+}
+
+template <s64 MacIndex>
+void Gte::check_mac_ovf_and_udf(s64 val) {
+  if (MacIndex == 1)
+    check_ovf_and_udf<43>(val, FlagRegister::MAC1_OVF_POS, FlagRegister::MAC1_OVF_NEG);
+  else if (MacIndex == 2)
+    check_ovf_and_udf<43>(val, FlagRegister::MAC2_OVF_POS, FlagRegister::MAC2_OVF_NEG);
+  else if (MacIndex == 3)
+    check_ovf_and_udf<43>(val, FlagRegister::MAC3_OVF_POS, FlagRegister::MAC3_OVF_NEG);
+}
+
+template <s64 MacIndex>
+s64 Gte::check_mac_ovf_and_extend(s64 val) {
+  check_mac_ovf_and_udf<MacIndex>(val);
+
+  return bit_utils::sign_extend<43, s64>(val);
+}
+
+void Gte::mul_vec_vec(vec3_s16 v1, vec3_s16 v2, vec3_s16 tr) {
+  set_mac_and_ir<1>(((s64)tr.x << 12) + v1.x * v2.x, m_lm);
+  set_mac_and_ir<2>(((s64)tr.y << 12) + v1.y * v2.y, m_lm);
+  set_mac_and_ir<3>(((s64)tr.z << 12) + v1.z * v2.z, m_lm);
+}
+
+template <bool Rtp>
+vec3_s64 Gte::mul_mat_vec(mat3x3_s16 mat, vec3_s16 vec, vec3_s32 tr) {
+  vec3_s64 product;
+
+  product.x = check_mac_ovf_and_extend<1>(
+      check_mac_ovf_and_extend<1>(check_mac_ovf_and_extend<1>(((s64)tr.x << 12) + mat[0][0] * vec.x) +
+                                  mat[0][1] * vec.y) +
+      mat[0][2] * vec.z);
+  product.y = check_mac_ovf_and_extend<2>(
+      check_mac_ovf_and_extend<2>(check_mac_ovf_and_extend<2>(((s64)tr.y << 12) + mat[1][0] * vec.x) +
+                                  mat[1][1] * vec.y) +
+      mat[1][2] * vec.z);
+  product.z = check_mac_ovf_and_extend<3>(
+      check_mac_ovf_and_extend<3>(check_mac_ovf_and_extend<3>(((s64)tr.z << 12) + mat[2][0] * vec.x) +
+                                  mat[2][1] * vec.y) +
+      mat[2][2] * vec.z);
+
+  set_mac_and_ir<1>(product.x, m_lm);
+  set_mac_and_ir<2>(product.y, m_lm);
+  if (!Rtp)
+    set_mac_and_ir<3>(product.z, m_lm);
+  else
+    set_mac<3>(product.z);
+  return product;
+}
+
+s64 Gte::mul_mat_vec_rtp(mat3x3_s16 mat, vec3_s16 vec, vec3_s32 tr) {
+  const auto product = mul_mat_vec<true>(mat, vec, tr);
+
+  // RTP calculates the IR3 saturation flag as if the lm bit is false
+  Gte::clamp((s32)(product.z >> 12), -0x8000, 0x7FFF, FlagRegister::IR3_SAT);
+
+  // But the calculation itself respects the lm bit
+  ir[3] = std::clamp(mac[3], m_lm ? 0 : -0x8000, 0x7FFF);
+
+  return product.z;
+}
+
+template <s32 IrIndex>
+void Gte::set_ir(s32 val, bool lm) {
+  u32 sat_bit{};
+  if (IrIndex == 1)
+    sat_bit = FlagRegister::IR1_SAT;
+  else if (IrIndex == 2)
+    sat_bit = FlagRegister::IR2_SAT;
+  else if (IrIndex == 3)
+    sat_bit = FlagRegister::IR3_SAT;
+  ir[IrIndex] = Gte::clamp(val, lm ? 0 : -0x8000, 0x7FFF, sat_bit);
+}
+
+template <s32 MacIndex>
+s64 Gte::set_mac(s64 val) {
+  check_mac_ovf_and_udf<MacIndex>(val);
+
+  if (m_sf)
+    val >>= 12;
+
+  mac[MacIndex] = (s32)val;
+  return val;
+}
+
+template <>
+s64 Gte::set_mac<0>(s64 val) {
+  check_ovf_and_udf<31>(val, FlagRegister::MAC0_OVF_POS, FlagRegister::MAC0_OVF_NEG);
+
+  mac[0] = (s32)val;
+  return val;
+}
+
+template <int MacIrIndex>
+void Gte::set_mac_and_ir(s64 val, bool lm) {
+  set_ir<MacIrIndex>((s32)set_mac<MacIrIndex>(val), lm);
+}
+
+void Gte::set_otz(s64 val) {
+  avg_z = (u16)Gte::clamp((s32)(val >> 12), 0, 0xFFFF, FlagRegister::SZ3_OTZ_SAT);
+}
+
+void Gte::push_screen_xy(s32 x, s32 y) {
+  s_xy[0].x = s_xy[1].x;
+  s_xy[0].y = s_xy[1].y;
+  s_xy[1].x = s_xy[2].x;
+  s_xy[1].y = s_xy[2].y;
+
+  s_xy[2].x = clamp(x, -0x400, 0x3FF, FlagRegister::SX2_SAT);
+  s_xy[2].y = clamp(y, -0x400, 0x3FF, FlagRegister::SY2_SAT);
+}
+
+void Gte::push_screen_z(s32 z) {
+  s_z[0] = s_z[1];
+  s_z[1] = s_z[2];
+  s_z[2] = s_z[3];
+
+  s_z[3] = clamp(z, 0, 0xFFFF, FlagRegister::SZ3_OTZ_SAT);
+}
+
+u32 Gte::recip(u16 divisor) {
+  s32 x = UNR_COUNT + UNR_TABLE[((divisor & 0x7FFF) + 0x40) >> 7];
+
+  s32 tmp = (((s32)divisor * -x) + 0x80) >> 8;
+  s32 tmp2 = ((x * (0x20000 + tmp)) + 0x80) >> 8;
+
+  return tmp2;
+}
+
+u32 Gte::divide_unr(u32 lhs, u32 rhs) {
+  if (rhs * 2 <= lhs) {
+    flag.div_ovf = true;
+    return DIVIDE_OVERFLOW_VAL;
+  }
+
+  const auto shift = bit_utils::leading_zeroes<u16>(rhs);
+  lhs <<= shift;
+  rhs <<= shift;
+
+  const auto reciprocal = recip(rhs | 0x8000);
+
+  const u32 result = ((u64)lhs * reciprocal + 0x8000) >> 16;
+
+  if (result > DIVIDE_OVERFLOW_VAL) {
+    flag.div_ovf = true;
+    return DIVIDE_OVERFLOW_VAL;
+  }
+
+  return result;
+}
+
+void Gte::push_color(u32 r, u32 g, u32 b) {
+  rgb_fifo[0] = rgb_fifo[1];
+  rgb_fifo[1] = rgb_fifo[2];
+
+  rgb_fifo[2].r = Gte::clamp(r, 0, 0xFF, FlagRegister::COLOR_R_SAT);
+  rgb_fifo[2].g = Gte::clamp(g, 0, 0xFF, FlagRegister::COLOR_G_SAT);
+  rgb_fifo[2].b = Gte::clamp(b, 0, 0xFF, FlagRegister::COLOR_B_SAT);
+  rgb_fifo[2].a = rgbc.a;
+}
+void Gte::push_color() {
+  push_color(mac[1] >> 4, mac[2] >> 4, mac[3] >> 4);
+}
+
 void Gte::cmd(u32 word) {
   gte::GteCommand cmd(word);
 
-  LOG_DEBUG_GTE("COP2: {:<4}  | 0x{:08X} at 0x{:08X}", cmd.to_str(), word, m_cpu.current_pc());
-
   flag.reset();
+  m_sf = cmd.sf;
+  m_lm = cmd.lm;
+
+  LOG_DEBUG_GTE("COP2: {:<4}  | 0x{:08X} at 0x{:08X}", cmd.to_str(), word, m_cpu.current_pc());
 
   switch (cmd.opcode()) {
     case GteCommand::RTPS: cmd_rtps(); break;
@@ -252,7 +439,7 @@ void Gte::cmd(u32 word) {
     case GteCommand::OP: cmd_op(); break;
     case GteCommand::DPCS: cmd_dpcs(); break;
     case GteCommand::INTPL: cmd_intpl(); break;
-    case GteCommand::MVMVA: cmd_mvmva(); break;
+    case GteCommand::MVMVA: cmd_mvmva(cmd.mvmva_mul_mat, cmd.mvmva_mul_vec, cmd.mvmva_trans); break;
     case GteCommand::NCDS: cmd_ncds(); break;
     case GteCommand::CDP: cmd_cdp(); break;
     case GteCommand::NCDT: cmd_ncdt(); break;
@@ -272,28 +459,177 @@ void Gte::cmd(u32 word) {
   }
 }
 
-void Gte::cmd_rtps() {}
-void Gte::cmd_nclip() {}
+void Gte::cmd_rtps(u8 vec_idx, bool set_mac0) {
+  s64 mac3 = mul_mat_vec_rtp(rot_mat, v[vec_idx], trans_vec);
+
+  push_screen_z((s32)(mac3 >> 12));
+  s64 h_s3z = divide_unr(h, s_z[3]);
+
+  s32 x = (s32)(set_mac<0>((s64)(h_s3z * ir[1]) + screen_offset[0]) >> 16);
+  s32 y = (s32)(set_mac<0>(h_s3z * ir[2] + screen_offset[1]) >> 16);
+  push_screen_xy(x, y);
+
+  if (set_mac0) {
+    s64 mac0 = set_mac<0>(h_s3z * dqa + dqb);
+    ir[0] = Gte::clamp((s32)(mac0 >> 12), 0, 0x1000, FlagRegister::IR0_SAT);
+  }
+}
+
+void Gte::cmd_nclip() {
+  set_mac<0>((s64)s_xy[0].x * s_xy[1].y + s_xy[1].x * s_xy[2].y + s_xy[2].x * s_xy[0].y -
+             s_xy[0].x * s_xy[2].y - s_xy[1].x * s_xy[0].y - s_xy[2].x * s_xy[1].y);
+}
+
 void Gte::cmd_op() {}
-void Gte::cmd_dpcs() {}
+
+void Gte::cmd_dpcs(bool use_rgb0) {
+  s16 r = use_rgb0 ? rgb_fifo[0].r << 4 : rgbc_r();
+  s16 g = use_rgb0 ? rgb_fifo[0].g << 4 : rgbc_g();
+  s16 b = use_rgb0 ? rgb_fifo[0].b << 4 : rgbc_b();
+
+  set_mac_and_ir<1>(((s64)far_color.r << 12) - (r << 12));
+  set_mac_and_ir<2>(((s64)far_color.r << 12) - (g << 12));
+  set_mac_and_ir<3>(((s64)far_color.b << 12) - (b << 12));
+
+  mul_vec_vec(ir0_to_vec(), ir_to_vec(), vec3_s16(r, g, b));
+  push_color();
+}
+
 void Gte::cmd_intpl() {}
-void Gte::cmd_mvmva() {}
-void Gte::cmd_ncds() {}
+
+void Gte::cmd_mvmva(u32 mul_mat_idx, u32 mul_vec_idx, u32 tr_vec_idx) {
+  mat3x3_s16 mul_mat;
+  if (mul_mat_idx == 0)
+    mul_mat = rot_mat;
+  else if (mul_mat_idx == 0)
+    mul_mat = rot_mat;
+  else if (mul_mat_idx == 0)
+    mul_mat = rot_mat;
+  else {
+    mul_mat = {};
+    LOG_ERROR_GTE("Buggy matrix selected");
+  }
+
+  vec3_s16 mul_vec;
+  if (mul_vec_idx == 0)
+    mul_vec = v[0];
+  else if (mul_vec_idx == 1)
+    mul_vec = v[1];
+  else if (mul_vec_idx == 2)
+    mul_vec = v[2];
+  else {
+    mul_vec.x = ir[1];
+    mul_vec.y = ir[2];
+    mul_vec.z = ir[3];
+  }
+  vec3_s32 tr_vec;
+  if (tr_vec_idx == 0)
+    tr_vec = trans_vec;
+  else if (tr_vec_idx == 1)
+    tr_vec = bg_col;
+  else if (tr_vec_idx == 2) {
+    tr_vec = {};
+    LOG_ERROR_GTE("Buggy FarColor vector operation");
+  } else
+    tr_vec = {};
+
+  mul_mat_vec(mul_mat, mul_vec, tr_vec);
+}
+
+void Gte::cmd_ncds(u8 vec_idx) {
+  mul_mat_vec(light_mat, v[vec_idx]);
+
+  mul_mat_vec(light_col_src_mat, ir_to_vec(), bg_col);
+
+  const auto ir_vec_tmp = ir_to_vec();
+
+  set_mac_and_ir<1>(((s64)far_color.r << 12) - (rgbc_r() * ir[1]));
+  set_mac_and_ir<2>(((s64)far_color.g << 12) - (rgbc_g() * ir[2]));
+  set_mac_and_ir<3>(((s64)far_color.b << 12) - (rgbc_b() * ir[3]));
+
+  set_mac_and_ir<1>((rgbc_r() * ir_vec_tmp.x) + ir[0] * ir[1], m_lm);
+  set_mac_and_ir<2>((rgbc_g() * ir_vec_tmp.y) + ir[0] * ir[2], m_lm);
+  set_mac_and_ir<3>((rgbc_b() * ir_vec_tmp.z) + ir[0] * ir[3], m_lm);
+
+  push_color();
+}
+
 void Gte::cmd_cdp() {}
-void Gte::cmd_ncdt() {}
-void Gte::cmd_nccs() {}
-void Gte::cmd_cc() {}
-void Gte::cmd_ncs() {}
-void Gte::cmd_nct() {}
-void Gte::cmd_sqr() {}
+
+void Gte::cmd_ncdt() {
+  cmd_ncds(0);
+  cmd_ncds(1);
+  cmd_ncds(2);
+}
+
+void Gte::cmd_nccs(u8 vec_idx) {
+  mul_mat_vec(light_mat, v[vec_idx]);
+  mul_mat_vec(light_col_src_mat, ir_to_vec(), bg_col);
+  mul_vec_vec(rgbc_to_vec(), ir_to_vec());
+  push_color();
+}
+
+void Gte::cmd_cc() {
+  mul_mat_vec(light_col_src_mat, ir_to_vec(), bg_col);
+  mul_vec_vec(rgbc_to_vec(), ir_to_vec());
+  push_color();
+}
+
+void Gte::cmd_ncs(u8 vec_idx) {
+  mul_mat_vec(light_mat, v[vec_idx]);
+  mul_mat_vec(light_col_src_mat, ir_to_vec(), bg_col);
+  push_color();
+}
+
+void Gte::cmd_nct() {
+  cmd_ncs(0);
+  cmd_ncs(1);
+  cmd_ncs(2);
+}
+
+void Gte::cmd_sqr() {
+  mul_vec_vec(ir_to_vec(), ir_to_vec());
+}
+
 void Gte::cmd_dcpl() {}
-void Gte::cmd_dpct() {}
-void Gte::cmd_avsz3() {}
-void Gte::cmd_avsz4() {}
-void Gte::cmd_rtpt() {}
-void Gte::cmd_gpf() {}
-void Gte::cmd_gpl() {}
-void Gte::cmd_ncct() {}
+
+void Gte::cmd_dpct() {
+  cmd_dpcs(true);
+  cmd_dpcs(true);
+  cmd_dpcs(true);
+}
+
+void Gte::cmd_avsz3() {
+  set_otz(set_mac<0>((s64)zsf3 * (s_z[1] + s_z[2] + s_z[3])));
+}
+
+void Gte::cmd_avsz4() {
+  set_otz(set_mac<0>((s64)zsf4 * (s_z[0] + s_z[1] + s_z[2] + s_z[3])));
+}
+
+void Gte::cmd_rtpt() {
+  cmd_rtps(0, false);
+  cmd_rtps(1, false);
+  cmd_rtps(2);
+}
+
+void Gte::cmd_gpf() {
+  mul_vec_vec(ir0_to_vec(), ir_to_vec());
+  push_color();
+}
+
+void Gte::cmd_gpl() {
+  set_mac_and_ir<1>(((s64)mac[1] << (m_sf * 12)) + ir[0] * ir[1], m_lm);
+  set_mac_and_ir<2>(((s64)mac[2] << (m_sf * 12)) + ir[0] * ir[2], m_lm);
+  set_mac_and_ir<3>(((s64)mac[3] << (m_sf * 12)) + ir[0] * ir[3], m_lm);
+  push_color();
+}
+
+void Gte::cmd_ncct() {
+  cmd_nccs(0);
+  cmd_nccs(1);
+  cmd_nccs(2);
+}
 
 }  // namespace gte
 }  // namespace cpu
--- src/cpu/gte.hpp
+++ src/cpu/gte.hpp
@@ -5,6 +5,8 @@
 #include <gsl-lite.hpp>
 #include <util/types.hpp>
 
+#include <array>
+
 namespace cpu {
 class Cpu;
 class Instruction;
@@ -17,11 +19,15 @@ using vec2_s16 = glm::vec<2, s16, glm::defaultp>;
 using vec2_s32 = glm::vec<2, s32, glm::defaultp>;
 using vec3_s16 = glm::vec<3, s16, glm::defaultp>;
 using vec3_s32 = glm::vec<3, s32, glm::defaultp>;
+using vec3_s64 = glm::vec<3, s64, glm::defaultp>;
 
 using mat3x3_s16 = glm::mat<3, 3, s16, glm::defaultp>;
 
 using col4_u8 = glm::vec<4, u8, glm::defaultp>;
 using col3_s32 = glm::vec<3, s32, glm::defaultp>;
+
+constexpr auto DIVIDE_OVERFLOW_VAL = 0x1FFFF;
+constexpr auto UNR_COUNT = 0x101;
 
 inline col4_u8 col4_from_word(u32 val) {
   // TODO: verify endianness
@@ -37,9 +43,17 @@ inline u32 col4_to_word(col4_u8 c) {
   return (u32)c.r | (u32)c.g << 8 | (u32)c.b << 16 | (u32)c.a << 24;
 }
 
+constexpr std::array<u8, UNR_COUNT> generate_unr_table() {
+  std::array<u8, UNR_COUNT> table = { { 0 } };
+  for (auto i = 0; i < (s32)table.size(); i++) {
+    table[i] = std::max(0, (0x40000 / (i + 0x100) + 1) / 2 - UNR_COUNT);
+  }
+  return table;
+}
+
 class Gte {
  public:
-  explicit Gte(cpu::Cpu& cpu) : m_cpu(cpu) {}
+  explicit Gte(cpu::Cpu& cpu) : UNR_TABLE(generate_unr_table()), m_cpu(cpu) {}
 
   u32 read_reg(u32 src_reg);
   void write_reg(u32 dest_reg, u32 val);
@@ -47,6 +61,29 @@ class Gte {
 
  private:
   union FlagRegister {
+    enum {
+      IR0_SAT = 1 << 12,
+      SY2_SAT = 1 << 13,
+      SX2_SAT = 1 << 14,
+      MAC0_OVF_NEG = 1 << 15,
+      MAC0_OVF_POS = 1 << 16,
+      DIVIDE_OVF = 1 << 17,
+      SZ3_OTZ_SAT = 1 << 18,
+      COLOR_B_SAT = 1 << 19,
+      COLOR_G_SAT = 1 << 20,
+      COLOR_R_SAT = 1 << 21,
+      IR3_SAT = 1 << 22,
+      IR2_SAT = 1 << 23,
+      IR1_SAT = 1 << 24,
+      MAC3_OVF_NEG = 1 << 25,
+      MAC2_OVF_NEG = 1 << 26,
+      MAC1_OVF_NEG = 1 << 27,
+      MAC3_OVF_POS = 1 << 28,
+      MAC2_OVF_POS = 1 << 29,
+      MAC1_OVF_POS = 1 << 30,
+      FLAG = 1 << 31
+    };
+
     u32 word{};
 
     struct {
@@ -81,29 +118,79 @@ class Gte {
     }
   };
 
+  //
+  // Command helpers
+  //
+
+  s32 clamp(s32 val, s32 min, s32 max, u32 flags = 0);
+
+  // Check for overflow/underflow and set appropriate bits if any
+  template <s32 BitSize>
+  void check_ovf_and_udf(s64 val, u32 ovf_bit, u32 udf_bit);
+  template <s64 MacIndex>
+  void check_mac_ovf_and_udf(s64 val);
+  template <s64 MacIndex>
+  s64 check_mac_ovf_and_extend(s64 val);
+
+  void mul_vec_vec(vec3_s16 v1, vec3_s16 v2, vec3_s16 tr = {});
+  template <bool Rtp = false>
+  vec3_s64 mul_mat_vec(mat3x3_s16 mat, vec3_s16 vec, vec3_s32 tr = {});
+  s64 mul_mat_vec_rtp(mat3x3_s16 mat, vec3_s16 vec, vec3_s32 tr);
+
+  template <s32 IrIndex>
+  void set_ir(s32 val, bool lm = false);
+  template <s32 MacIndex>
+  s64 set_mac(s64 val);
+  template <s32 MacIrIndex>
+  void set_mac_and_ir(s64 val, bool lm = false);
+  void set_otz(s64 val);
+
+  void push_screen_xy(s32 x, s32 y);
+  void push_screen_z(s32 z);
+  void push_color(u32 r, u32 g, u32 b);
+  void push_color();
+
+  u32 recip(u16 divisor);
+  u32 divide_unr(u32 lhs, u32 rhs);
+
+  s16 rgbc_r() const { return rgbc.r << 4; }
+  s16 rgbc_g() const { return rgbc.g << 4; }
+  s16 rgbc_b() const { return rgbc.b << 4; }
+  vec3_s16 ir_to_vec() const { return { ir[1], ir[2], ir[3] }; }
+  vec3_s16 ir0_to_vec() const { return { ir[0], ir[0], ir[0] }; }
+  vec3_s16 rgbc_to_vec() const { return { rgbc_r(), rgbc_g(), rgbc_b() }; }
+
+  bool m_sf{};
+  bool m_lm{};
+  const std::array<u8, UNR_COUNT> UNR_TABLE;
+
+  //
   // Commands
-  void cmd_rtps();
-  void cmd_nclip();
+  //
+
+  void cmd_rtps(u8 vec_n = 0,
+                bool set_mac0 = true);  // Coordinate transformation and perspective transformation
+  void cmd_nclip();                     // Normal clipping
   void cmd_op();
-  void cmd_dpcs();
+  void cmd_dpcs(bool use_rgb0 = false);
   void cmd_intpl();
-  void cmd_mvmva();
-  void cmd_ncds();
+  void cmd_mvmva(u32 mul_mat, u32 mul_vec, u32 trans_vec);  // Matrix and vector multiplication
+  void cmd_ncds(u8 vec_idx = 0);                            // Light source calculation
   void cmd_cdp();
-  void cmd_ncdt();
-  void cmd_nccs();
-  void cmd_cc();
-  void cmd_ncs();
+  void cmd_ncdt();                // Light source calculation
+  void cmd_nccs(u8 vec_idx = 0);  // Light source calculation
+  void cmd_cc();                  // Light source calculation
+  void cmd_ncs(u8 vec_idx = 0);
   void cmd_nct();
   void cmd_sqr();
   void cmd_dcpl();
   void cmd_dpct();
-  void cmd_avsz3();
-  void cmd_avsz4();
-  void cmd_rtpt();
-  void cmd_gpf();
-  void cmd_gpl();
-  void cmd_ncct();
+  void cmd_avsz3();  // Calculate average of 3 Z values
+  void cmd_avsz4();  // Calculate average of 4 Z values
+  void cmd_rtpt();   // Coordinate transformation and perspective transformation
+  void cmd_gpf();    // General purpose interpolation
+  void cmd_gpl();    // General purpose interpolation
+  void cmd_ncct();   // Light source calculation
 
  private:
   //
@@ -111,18 +198,18 @@ class Gte {
   //
 
   // Data Registers
-  vec3_s16 v[3];        // [0-5] Vector 0 - 3
-  col4_u8 rgbc;         // [6] Color/code values
-  u16 avg_z;            // [7] Average Z values (for Ordering Table)
-  s16 ir[4];            // [8-11] 16bit Accumulator (0: Interpolate, 1-3: Vector)
-  vec2_s16 s_xy[4];     // [12-15] Screen XY-coordinate FIFO  (3 stages)
-  u16 s_z[4];           // [16-19] Screen Z-coordinate FIFO   (4 stages)
-  col4_u8 rgb_fifo[3];  // [20-22] Color CRGB-code/color FIFO (3 stages)
-  col4_u8 res;          // [23] Prohibited (?)
-  s32 mac[4];           // [24-27] 32bit Maths Accumulators (0: Value, 1-3: Vector)
-  u16 rgb_conv;         // [28-29] Convert RGB Color (48bit vs 15bit)
-  s32 lzcs;             // Count Leading-Zeroes/Ones (sign bits)
-  s32 lzcr;             // Count Leading-Zeroes/Ones (sign bits)
+  vec3_s16 v[3]{};        // [0-5] Vector 0 - 3
+  col4_u8 rgbc{};         // [6] Color/code values
+  u16 avg_z{};            // [7] Average Z values (for Ordering Table)
+  s16 ir[4]{};            // [8-11] 16bit Accumulator (0: Interpolate, 1-3: Vector)
+  vec2_s16 s_xy[4]{};     // [12-15] Screen XY-coordinate FIFO  (3 stages)
+  u16 s_z[4]{};           // [16-19] Screen Z-coordinate FIFO   (4 stages)
+  col4_u8 rgb_fifo[3]{};  // [20-22] Color CRGB-code/color FIFO (3 stages)
+  u32 res{};              // [23] Prohibited (?)
+  s32 mac[4]{};           // [24-27] 32bit Maths Accumulators (0: Value, 1-3: Vector)
+  u16 rgb_conv{};         // [28-29] Convert RGB Color (48bit vs 15bit)
+  s32 lzcs{};             // Count Leading-Zeroes/Ones (sign bits)
+  s32 lzcr{};             // Count Leading-Zeroes/Ones (sign bits)
 
   // Control Registers
   mat3x3_s16 rot_mat{};            // [0-4] Rotation matrix
